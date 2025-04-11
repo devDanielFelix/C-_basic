34ab9ea2704d5ee262e78df349c7a1f74c18b302
Daniel Felix <102247488+devDanielFelix@users.noreply.github.com>
2025-04-11 19:26:32 -0300

    Add files via upload

---
+++ Estrutura_de_repetição/Estrutura de repetição com goto/Estrutura de repetição com goto/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("## Estrutura de repetição com goto/label ##");
+
+int i = 1;
+
+repetir:
+Console.WriteLine($"i= {i}");
+
+i++;
+
+if (i <= 10)
+    goto repetir;
+
+Console.WriteLine("Fim do processamento");
+Console.ReadKey();
