5fa677ceedb2b06b41af2b79179a8419e3d87895
Daniel Felix <102247488+devDanielFelix@users.noreply.github.com>
2025-11-01 16:50:47 -0300

    Refactor FizzBuzz implementation for clarity

--- DesafioFizzBuzz/DesafioFizzBuzz/Program.cs
+++ DesafioFizzBuzz/DesafioFizzBuzz/Program.cs
@@ -2,9 +2,7 @@
 Console.WriteLine("## Desafio FizzBuzz ##\n");
 Console.ReadKey();
 
-int i = 1;
-
-for ( i = 1; i < 30; i++) 
+for (int i = 1; i < 30; i++) 
 {
   if (i % 3 == 0 && i % 5 == 0)
     {
@@ -23,4 +21,5 @@ for ( i = 1; i < 30; i++)
         Console.WriteLine(i);
     }  
 }
+
 Console.ReadKey();
