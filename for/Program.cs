75dcf31725e1bc4cfbed4b19198de21792db57c1
Daniel Felix <102247488+devDanielFelix@users.noreply.github.com>
2025-11-01 16:39:10 -0300

    Add files via upload

---
+++ for/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("## estrutura de repetição for ##");
+
+int numero, resultado;
+
+Console.Write("Digite um número para ver a tabuada: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 0)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        resultado = numero * i;
+        Console.WriteLine($"{numero} x {i} = {resultado}");
+    }
+}
+else
+{
+    Console.WriteLine("Número inválido.");
+}
