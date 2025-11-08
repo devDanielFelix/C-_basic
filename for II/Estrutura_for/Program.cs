bc404a58aaeea9481e604d471180f5dbce355e10
Daniel Felix <102247488+devDanielFelix@users.noreply.github.com>
2025-11-08 15:03:47 -0300

    Add files via upload

---
+++ for II/Estrutura_for/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("## Estrutura FOR ##");
+
+for (int x = 0; x<=5; x++)
+{
+    for (int y = 0; y<=5; y++)
+    {
+        Console.Write($"({x}, {y}) ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Fim do Procesamento!");
+Console.ReadKey();
